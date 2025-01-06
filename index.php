d1c69e659655f4518f04318876287d35dbd11562
AVTAVANTTOUT <elias.bouzekouk@ecoles-epsi.net>
2025-01-06 20:39:48 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,7 @@
+<?php
+include 'config.php';
+$taches = $conn->query("SELECT * FROM taches")->fetchAll(PDO::FETCH_ASSOC);
+?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -9,11 +13,6 @@
   <H1>DailyDone</H1>
 <div class="container">
     <a href="ajouter.php">Ajouter une tâche</a>
-    <ul>
-        <?php
-include 'config.php';
-$taches = $conn->query("SELECT * FROM taches")->fetchAll(PDO::FETCH_ASSOC);
-?>
     <ul>
     <?php foreach ($taches as $t): ?>
       <li><?= $t['titre']; ?> - <?= $t['description']; ?> <a href="modifier.php?id=<?= $t['id']; ?>">Modifier</a> <a href="supprimer.php?id=<?= $t['id']; ?>">Supprimer</a></li>
